427bd01c1bd8e41e54de1fd1d18558176f0b157d
solufa <solufa2020@gmail.com>
2025-02-23 19:07:58 +0900

    chore: remove Config type

--- src/getConfig.ts
+++ src/getConfig.ts
@@ -1,11 +1,11 @@
 import fs from 'fs';
 import path from 'path';
 
-export type Config = {
+export const getConfig = (
+  dir = process.cwd(),
+): {
   appDir: string | undefined;
-};
-
-export const getConfig = (dir = process.cwd()): Config => {
+} => {
   const srcDir = fs.existsSync(path.posix.join(dir, 'src/app')) ? path.posix.join(dir, 'src') : dir;
   const appDir = path.posix.join(srcDir, 'app');
   const isAppDirUsed = fs.existsSync(appDir);
--- src/initTSC.ts
+++ src/initTSC.ts
@@ -3,7 +3,6 @@ import ts from 'typescript';
 export const initTSC = (frourioFiles: string[]) => {
   const configDir = process.cwd();
   const configFileName = ts.findConfigFile(configDir, ts.sys.fileExists);
-
   const compilerOptions = configFileName
     ? ts.parseJsonConfigFileContent(
         ts.readConfigFile(configFileName, ts.sys.readFile).config,

eb4d1f5f46ae602d38f95487dd5916174a9ee9ba
solufa <solufa2020@gmail.com>
2025-02-26 18:21:10 +0900

    fix: update sorting logic of frourio dir names

--- src/listFrourioFiles.ts
+++ src/listFrourioFiles.ts
@@ -2,17 +2,20 @@ import { readdirSync } from 'fs';
 import path from 'path';
 import { FROURIO_FILE } from './constants';
 
+const listDirNames = (dir: string): string[] =>
+  readdirSync(dir, { withFileTypes: true }).reduce<string[]>(
+    (prev, d) => [
+      ...prev,
+      ...(d.isFile() && d.name === FROURIO_FILE
+        ? [dir.replaceAll('\\', '/')]
+        : !d.isDirectory() || d.name.startsWith('_')
+          ? []
+          : listDirNames(path.posix.join(dir, d.name))),
+    ],
+    [],
+  );
+
 export const listFrourioFiles = (dir: string): string[] =>
-  readdirSync(dir, { withFileTypes: true })
-    .reduce<string[]>(
-      (prev, d) => [
-        ...prev,
-        ...(d.isFile() && d.name === FROURIO_FILE
-          ? [path.posix.join(dir.replaceAll('\\', '/'), d.name)]
-          : !d.isDirectory() || d.name.startsWith('_')
-            ? []
-            : listFrourioFiles(path.posix.join(dir, d.name))),
-      ],
-      [],
-    )
-    .sort();
+  listDirNames(dir)
+    .sort()
+    .map((f) => path.posix.join(f, FROURIO_FILE));
